3ac216f36f0a5d96773b67166fa9ac0ac799ef4e
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-06 00:33:48 +0430

    :card_file_box: Adding Category and product models

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse('product_by_category', kwargs={'slug': self.slug})
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products_images')
+    number_of_product = models.PositiveSmallIntegerField()
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    insertion_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        ordering = ['insertion_date']
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'slug': self.slug})
