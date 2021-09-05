29b7a0649ad63b73c589e128f6eb4efd6f5da726
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-06 00:37:58 +0430

    :poop: Adding products view

--- store/products/views.py
+++ store/products/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import (ListView, DetailView)
+from .models import (Product)
 
-# Create your views here.
+
+class ProductList(ListView):
+    model = Product
+    queryset = Product.objects.all(is_active=True)
+    context_object_name = 'products'
+    template_name = 'products/index.html'
+
+
+class ProductDetail(DetailView):
+    model = Product
+    queryset = Product.objects.all(is_active=True)
+    context_object_name = 'product'
+    template_name = 'products/product_detail.html'
+
+
+class ProductByCategory(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'products/product_by_category.html'
+    queryset = Product.objects.all(is_active=True)
+
+    def get_queryset(self):
+        """Filter products by a category"""
+        return self.queryset.filter(category=self.kwargs.get('slug'))
