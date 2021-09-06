5fa4ec724cf976b3ef1d3ca2be79caef6697a2aa
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-06 14:05:58 +0430

    :truck: Change one route and several query sets in products views

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import (ProductList, ProductDetail, ProductByCategory)
 
 app_name = 'products'
 
-urlpatterns = []
+urlpatterns = [
+    path('', ProductList.as_view(), 'product_list'),
+    path('products/<slug:slug>', ProductDetail.as_view(), 'product_detail'),
+    path('category/<slug:slug>', ProductByCategory.as_view(), 'product_by_category'),
+]
--- store/products/views.py
+++ store/products/views.py
@@ -5,14 +5,14 @@ from .models import (Product)
 
 class ProductList(ListView):
     model = Product
-    queryset = Product.objects.all(is_active=True)
+    queryset = Product.objects.filter(is_active=True)
     context_object_name = 'products'
     template_name = 'products/index.html'
 
 
 class ProductDetail(DetailView):
     model = Product
-    queryset = Product.objects.all(is_active=True)
+    queryset = Product.objects.filter(is_active=True)
     context_object_name = 'product'
     template_name = 'products/product_detail.html'
 
@@ -21,7 +21,7 @@ class ProductByCategory(DetailView):
     model = Product
     context_object_name = 'product'
     template_name = 'products/product_by_category.html'
-    queryset = Product.objects.all(is_active=True)
+    queryset = Product.objects.filter(is_active=True)
 
     def get_queryset(self):
         """Filter products by a category"""
