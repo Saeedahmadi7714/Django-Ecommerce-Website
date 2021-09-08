c948a7e3ba2f5960f01027fe5be6a9cb9a53135d
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-09 03:44:04 +0430

    :truck: Adding name to products/urls.py

--- store/products/urls.py
+++ store/products/urls.py
@@ -4,7 +4,7 @@ from .views import (ProductList, ProductDetail, ProductByCategory)
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductList.as_view(), 'product_list'),
-    path('products/<slug:slug>', ProductDetail.as_view(), 'product_detail'),
-    path('category/<slug:slug>', ProductByCategory.as_view(), 'product_by_category'),
+    path('', ProductList.as_view(), name='product_list'),
+    path('products/<slug:slug>', ProductDetail.as_view(), name='product_detail'),
+    path('category/<slug:slug>', ProductByCategory.as_view(), name='product_by_category'),
 ]
