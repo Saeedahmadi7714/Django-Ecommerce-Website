ce9cda77f9cb13281e228a12ca41659dce2cbd1d
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-13 00:06:54 +0430

    :wrench: Add / to customer url

--- store/conf/urls.py
+++ store/conf/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/v1/', include('api_core.api.urls')),
                   path('', include('products.urls')),
-                  path('customer', include('customers.urls')),
+                  path('customer/', include('customers.urls')),
                   path('order/', include('orders.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
