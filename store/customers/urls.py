7df8d18e01dc23d8b8da210d45cd73b3f7c65756
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-13 00:11:57 +0430

    :truck: Adding sign in/ sign up/ logout urls in customers app

--- store/customers/urls.py
+++ store/customers/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import (SignUpView, SignInView, )
 
 app_name = 'customers'
 
-urlpatterns = []
+urlpatterns = [
+    path('sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('sign_in/', SignInView.as_view(), name='sign_in'),
+]
