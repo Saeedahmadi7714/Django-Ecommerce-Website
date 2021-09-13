d18c625e2cc09a7537abd72a774f439726dae601
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-14 00:54:07 +0430

    :truck: Change customers url

--- store/customers/urls.py
+++ store/customers/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import (SignUpView, SignInView, LogoutView)
+from .views import (SignUpView, sign_in_view, logout_view, )
 
 app_name = 'customers'
 
 urlpatterns = [
     path('sign_up/', SignUpView.as_view(), name='sign_up'),
-    path('sign_in/', SignInView.as_view(), name='sign_in'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('sign_in/', sign_in_view, name='sign_in'),
+    path('logout/', logout_view, name='logout'),
 ]
