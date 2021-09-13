b5b1716a0690cd68eb548d83743f3274401fc4d2
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-14 00:55:03 +0430

    :passport_control: Complete user login and register and logout

--- store/customers/views.py
+++ store/customers/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import (authenticate, login, logout)
+from django.shortcuts import redirect, render
+from django.views.generic import FormView
 
-# Create your views here.
+from customers.forms import (SignUpForm, SignInForm, )
+
+
+class SignUpView(FormView):
+    form_class = SignUpForm
+    template_name = 'customers/sign_up.html'
+
+
+def sign_in_view(request):
+    if request.method == 'GET':
+        context = dict()
+        context['form'] = SignInForm
+        return render(request, 'customers/sign_in.html', context)
+
+    elif request.method == 'POST':
+
+        form = SignInForm(request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+
+                    login(request, user)
+                    return redirect('products:product_list')
+                else:
+
+                    messages.warning(request, "Your account is disabled!")
+                    return redirect('customers:sign_in')
+            else:
+                messages.warning(request, "The username or password are not valid!")
+
+        return redirect('customers:sign_in')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('products:product_list')
