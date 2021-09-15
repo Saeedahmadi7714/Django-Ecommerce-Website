03ec6401a9f85735c97406e40c4e1be18574f344
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-16 02:07:26 +0430

    :truck: Adding user profile view

--- store/customers/views.py
+++ store/customers/views.py
@@ -1,9 +1,10 @@
-from django.contrib import messages
-from django.contrib.auth import (authenticate, login, logout)
-from django.shortcuts import redirect, render
-from django.views.generic import FormView
-
-from customers.forms import (SignUpForm, SignInForm, )
+from django.contrib.auth import (logout, )
+from django.contrib.auth.decorators import (login_required, )
+from django.contrib.auth.views import (LoginView, )
+from django.shortcuts import (redirect, render, )
+from django.views.generic import (FormView, )
+from customers.forms import (SignUpForm, SignInForm, CustomerProfileForm, )
+from customers.models import (Customer, )
 
 
 class SignUpView(FormView):
@@ -11,37 +12,23 @@ class SignUpView(FormView):
     template_name = 'customers/sign_up.html'
 
 
-def sign_in_view(request):
+class SigninView(LoginView):
+    authentication_form = SignInForm
+    redirect_field_name = 'next'
+    template_name = 'customers/sign_in.html'
+    redirect_authenticated_user = True
+
+
+@login_required
+def customer_profile_view(request):
     if request.method == 'GET':
         context = dict()
-        context['form'] = SignInForm
-        return render(request, 'customers/sign_in.html', context)
+        context['form'] = CustomerProfileForm(instance=request.user)
+        return render(request, 'customers/customer_profile.html', context)
 
-    elif request.method == 'POST':
 
-        form = SignInForm(request.POST)
-
-        if form.is_valid():
-
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-
-                if user.is_active:
-
-                    login(request, user)
-                    return redirect('products:product_list')
-                else:
-
-                    messages.warning(request, "Your account is disabled!")
-                    return redirect('customers:sign_in')
-            else:
-                messages.warning(request, "The username or password are not valid!")
-
-        return redirect('customers:sign_in')
+def dashboard_view(request):
+    pass
 
 
 def logout_view(request):
