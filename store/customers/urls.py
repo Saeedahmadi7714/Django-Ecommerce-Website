09c526d919cc0ce42538f7b574e9bcadb018165f
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-16 02:07:03 +0430

    :truck: Update customers urls.py

--- store/customers/urls.py
+++ store/customers/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import (SignUpView, sign_in_view, logout_view, )
+from .views import (SignUpView, SigninView, logout_view, customer_profile_view, dashboard_view)
 
 app_name = 'customers'
 
 urlpatterns = [
     path('sign_up/', SignUpView.as_view(), name='sign_up'),
-    path('sign_in/', sign_in_view, name='sign_in'),
+    path('sign_in/', SigninView.as_view(), name='sign_in'),
+    path('profile/', customer_profile_view, name='profile'),
+    path('dashboard/', dashboard_view, name='dashboard'),
     path('logout/', logout_view, name='logout'),
 ]
