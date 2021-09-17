60932b25bfb783fc2b347439b8725f22adefb736
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-18 03:22:30 +0430

    :passport_control: Adding change password api url

--- store/customers/api/urls.py
+++ store/customers/api/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
-from .views import SignUp
+from django.urls import (path)
+
+from .views import (SignUp, ChangePassword)
 
 urlpatterns = [
-    path('sign_up/', SignUp.as_view(), name='sign_up_api')
+    path('sign_up/', SignUp.as_view(), name='sign_up_api'),
+    path('change_password/', ChangePassword.as_view(), name='change_password_api'),
 ]
