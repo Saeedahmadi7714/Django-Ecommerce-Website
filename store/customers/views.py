5c3904d9254c892421817a9de1bf8560ff110926
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-18 03:31:20 +0430

    :poop: Must be refactord.

--- store/customers/views.py
+++ store/customers/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import (logout, )
 from django.contrib.auth.decorators import (login_required, )
 from django.contrib.auth.views import (LoginView, )
+from django.contrib import (messages, )
+from django.http import (HttpResponse, )
 from django.shortcuts import (redirect, render, )
 from django.views.generic import (FormView, )
-from customers.forms import (SignUpForm, SignInForm, CustomerProfileForm, )
-from customers.models import (Customer, )
+from customers.forms import (SignUpForm, SignInForm,
+                             CustomerProfileForm, ChangePasswordForm, )
+from django.utils.translation import gettext_lazy as _
 
 
 class SignUpView(FormView):
@@ -19,12 +22,32 @@ class SigninView(LoginView):
     redirect_authenticated_user = True
 
 
+class ChangePasswordView(FormView):
+    form_class = ChangePasswordForm
+    template_name = 'customers/change_password.html'
+
+
 @login_required
 def customer_profile_view(request):
     if request.method == 'GET':
+
         context = dict()
         context['form'] = CustomerProfileForm(instance=request.user)
         return render(request, 'customers/customer_profile.html', context)
+
+    elif request.method == 'POST':
+
+        form = CustomerProfileForm(request.POST, request.FILES, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            print(request.user.username)
+            context = dict()
+            context['form'] = CustomerProfileForm(instance=request.user)
+            messages.success(request, _('Profile details updated successfully.'))
+            return render(request, 'customers/customer_profile.html', context)
+
+        return HttpResponse('Form invalid', form.errors)
 
 
 def dashboard_view(request):
@@ -33,4 +56,4 @@ def dashboard_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('products:product_list')
+    return redirect('products:index')
