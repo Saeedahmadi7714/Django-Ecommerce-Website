9c57055b6f69c00039a2aa6b1f432fa464df4021
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-18 03:27:07 +0430

    :lipstick: Update product style file

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import (ProductList, ProductDetail, ProductByCategory)
+from .views import (IndexView, ShopView, ProductDetail, ProductByCategory)
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductList.as_view(), name='product_list'),
+    path('', IndexView.as_view(), name='index'),
+    path('shop/', ShopView.as_view(), name='shop'),
     path('products/<slug:slug>', ProductDetail.as_view(), name='product_detail'),
     path('category/<slug:slug>', ProductByCategory.as_view(), name='product_by_category'),
 ]
