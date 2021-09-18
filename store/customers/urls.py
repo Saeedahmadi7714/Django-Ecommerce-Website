3776c3f8e048884c3a3090c868341c2c967f6fee
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-18 16:44:11 +0430

    :truck: Adding new url and change a url name

--- store/customers/urls.py
+++ store/customers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (SignUpView, SigninView, logout_view,
-                    customer_profile_view, dashboard_view, ChangePasswordView)
+                    customer_profile_view, orders_view,
+                    addresses_view, ChangePasswordView)
 
 app_name = 'customers'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('change_password/', ChangePasswordView.as_view(), name='change_password'),
     path('logout/', logout_view, name='logout'),
     path('profile/', customer_profile_view, name='profile'),
-    path('dashboard/', dashboard_view, name='dashboard'),
+    path('orders/', orders_view, name='orders'),
+    path('addresses/', addresses_view, name='addresses'),
 ]
