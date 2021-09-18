d1b37eccb87164c383b28be896522dd9f7086569
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-19 01:28:35 +0430

    :passport_control: Adding reset password urls.

--- store/conf/urls.py
+++ store/conf/urls.py
@@ -1,14 +1,35 @@
-from django.conf.urls.static import (static)
-from django.contrib import (admin)
-from django.urls import (path, include)
-
-from conf import (settings)
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from conf import settings
+from django.contrib.auth.views import (PasswordResetView,
+                                       PasswordResetDoneView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetCompleteView, )
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/v1/', include('api_core.api.urls')),
-                  path('', include('products.urls')),
-                  path('customer/', include('customers.urls')),
-                  path('order/', include('orders.urls')),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api_core.api.urls')),
+    path('', include('products.urls')),
+    path('customer/', include('customers.urls')),
+    path('order/', include('orders.urls')),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Django urls for reset password
+    path('password_reset/',
+         PasswordResetView.as_view(template_name='customers/password_reset.html'),
+         name='password_reset'),
+
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(template_name='customers/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='customers/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('reset/done/', PasswordResetCompleteView.as_view(template_name='customers/password_reset_complete.html'),
+         name='password_reset_complete'),
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
