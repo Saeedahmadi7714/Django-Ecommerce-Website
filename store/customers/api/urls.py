f592a4baa9430a723a2aac2c9297d65dafb4e85d
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-18 12:48:28 +0430

    :passport_control: Change view names

--- store/customers/api/urls.py
+++ store/customers/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import (path)
 
-from .views import (SignUp, ChangePassword)
+from .views import (SignUpView, ChangePasswordView)
 
 urlpatterns = [
-    path('sign_up/', SignUp.as_view(), name='sign_up_api'),
-    path('change_password/', ChangePassword.as_view(), name='change_password_api'),
+    path('sign_up/', SignUpView.as_view(), name='sign_up_api'),
+    path('change_password/', ChangePasswordView.as_view(), name='change_password_api'),
 ]
