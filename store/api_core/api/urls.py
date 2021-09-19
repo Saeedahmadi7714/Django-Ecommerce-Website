f1321438f51b0c99fb54114a426ed2f78e89bb35
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-19 16:29:51 +0430

    :alien: Include products api urls .

--- store/api_core/api/urls.py
+++ store/api_core/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('customers.api.urls'))
+    path('', include('customers.api.urls')),
+    path('', include('products.api.urls')),
 ]
