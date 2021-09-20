82960ddb753b0a8cc517e61f11eeed0ce8242e67
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-20 13:56:54 +0430

    :children_crossing: Contact form ajax added

--- store/products/static/products/js/custom.js
+++ store/products/static/products/js/custom.js
@@ -1,4 +1,4 @@
-// Send from data when user clicked on Sign up button
+// Sign up
 $(document).ready(function () {
 
     $("#signUpBtn").click(function (event) {
@@ -37,7 +37,7 @@ $(document).ready(function () {
 });
 
 
-// Send from data when user clicked on Change password button
+// Change password
 $(document).ready(function () {
 
     $("#changePasswordBtn").click(function (event) {
@@ -78,5 +78,55 @@ $(document).ready(function () {
         });
 
     });
+});
 
+
+// Send contact form
+$(document).ready(function () {
+
+    $("#letsTalkBtn").click(function (event) {
+
+        if (!$("#subject").val()) {
+            $(".alert").remove();
+            $("#error").prepend(`<div class="col-xl-12 alert alert-danger d-flex justify-content-center" role="alert">Please enter subject</div>`)
+        }
+
+        if (!$("#message").val()) {
+            $(".alert").remove();
+            $("#error").prepend(`<div class="col-xl-12 alert alert-danger d-flex justify-content-center" role="alert">Please enter message </div>`)
+        }
+
+
+        //Stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        // Get form
+        const form = $('#contactForm')[0];
+        // Create an FormData object
+        const data = new FormData(form);
+
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "/api/v1/contact/",
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function () {
+                console.log("SUCCESS :");
+                $(".alert").remove();
+                $("#error").prepend(`<div class="col-xl-12 alert alert-success d-flex justify-content-center" role="alert">Thanks for your message <p> &#128515;
+ </p> </div>`)
+
+            },
+            error: function (error) {
+                console.log("ERROR : ", error);
+
+            }
+        });
+
+    });
 });
