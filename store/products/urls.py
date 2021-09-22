de33eaa45c2e6adef55c3c902a4177894e7329a5
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-22 15:44:59 +0330

    Update urls

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (IndexView, ShopView,
-                    ProductDetail, ProductByCategory,
+                    ProductDetail, product_by_category,
                     AboutView, ContactView)
 
 app_name = 'products'
@@ -8,8 +8,8 @@ app_name = 'products'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('shop/', ShopView.as_view(), name='shop'),
-    path('products/<slug:slug>', ProductDetail.as_view(), name='product_detail'),
-    path('category/<slug:slug>', ProductByCategory.as_view(), name='product_by_category'),
+    path('products/<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
+    path('categories/<str:category>/', product_by_category, name='product_by_category'),
     path('about/', AboutView.as_view(), name='about'),
     path('contact/', ContactView.as_view(), name='contact'),
 ]
