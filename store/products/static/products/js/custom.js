f43140e13b83983a846790b10b2211419e88bbcd
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-25 13:57:14 +0330

    :sparkles: Load more ajax completed

--- store/products/static/products/js/custom.js
+++ store/products/static/products/js/custom.js
@@ -130,3 +130,86 @@ $(document).ready(function () {
 
     });
 });
+
+
+// Get data when user click on load more
+$(document).ready(function () {
+    let count = 0;
+
+    $("#loadMoreBtn").click(function (event) {
+        const category = $("#categoryP").text();
+        count += 1;
+
+        //Stop submit the form, we will post it manually.
+        event.preventDefault();
+
+
+        $.ajax({
+            type: "GET",
+            url: `/api/v1/product_list/?category=${category}&page=${count}`,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (response) {
+                console.log("SUCCESS :");
+
+                let result = response.results
+
+                for (let each in result) {
+
+                    $("#productRow").append(
+                        ` <div class="col-md-4">
+                            <div class="card mb-4 product-wap rounded-0">
+                                <div class="card rounded-0">
+                                    <img class="card-img rounded-0 img-fluid" src="${result[each]['image']}"
+                                         alt="{{ product.name }} Image">
+
+                                    <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                                        <ul class="list-unstyled">
+                                            <li><a class="btn btn-success text-white" href="shop-single.html"><i
+                                                    class="far fa-heart"></i></a></li>
+                                            <li><a class="btn btn-success text-white mt-2"
+                                                   href="{% url 'products:product_detail' product.slug %}"><i
+                                                    class="far fa-eye"></i></a></li>
+                                            <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i
+                                                    class="fas fa-cart-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="card-body">
+                                    <a href="{% url 'products:product_detail' product.slug %}"
+                                       class="h3 text-decoration-none">${result[each]['name']}</a>
+                                    <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                        <li class="pt-2">
+                                            <span class="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                                        </li>
+                                    </ul>
+                                    <p class="text-center mb-0">${result[each]['price']}</p>
+                                </div>
+                            </div>
+                        </div>
+`)
+
+                    console.log(result[each])
+                }
+
+                // console.log(e['results'])
+                // console.log(category)
+                // console.log(count)
+            },
+
+            error: function (error) {
+                console.log("ERROR : ", error);
+
+            }
+        });
+
+    });
+});
+
