f3fab8d60c958b5a3043019925ca18619e5f7beb
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-30 02:11:43 +0330

    :sparkles: Basket view added

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,1 +1,38 @@
 from django.shortcuts import render
+
+from products.models import Product
+
+
+def basket_view(request):
+    context = dict()
+    if request.method == 'GET':
+
+        if request.session.get('basket'):
+            basket = request.session['basket']
+            product_names = basket.keys()
+            context['products_in_basket'] = Product.objects.filter(name__in=product_names)
+            return render(request, 'orders/basket.html', context)
+        else:
+            return render(request, 'orders/basket.html')
+
+    product_name = request.POST.get('product_name')
+    product_quantity = request.POST.get('product_quantity')
+
+    if not request.session.get('basket'):
+
+        request.session['basket'] = {
+            product_name: product_quantity
+        }
+
+        print(request.session.get('basket'), 2)
+        context['products_in_basket'] = Product.objects.filter(name=product_name)
+        return render(request, 'orders/basket.html', context)
+
+    else:
+
+        basket = request.session['basket']
+        basket[product_name] = product_quantity
+        product_names = basket.keys()
+        print(basket, 3)
+        context['products_in_basket'] = Product.objects.filter(name__in=product_names)
+        return render(request, 'orders/basket.html', context)
