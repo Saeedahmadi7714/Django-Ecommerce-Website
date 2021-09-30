ca3e177f428f7954c722d249549586f86ce2ccbd
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-09-30 19:13:56 +0330

    :sparkles: Discount model added to order admin

--- store/orders/admin.py
+++ store/orders/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-from orders.models import Order, Discount
-
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    pass
+from orders.models import Discount
 
 
 @admin.register(Discount)
