c49ce5f573cf8fc6f39689cb7dcab306ac109793
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-10-01 01:33:28 +0330

    :construction: Working on delet item from basket view

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,10 +1,15 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
-
 from products.models import Product
+from django.views.decorators.csrf import csrf_exempt
 
 
 def basket_view(request):
+    """
+    Provides the ability to add products to the basket as well as view the basket
+    """
     context = dict()
     if request.method == 'GET':
 
@@ -14,13 +19,14 @@ def basket_view(request):
             context['products_in_basket'] = Product.objects.filter(name__in=product_names)
             return render(request, 'orders/basket.html', context)
         else:
+            # Empty basket
             return render(request, 'orders/basket.html')
 
     product_name = request.POST.get('product_name')
     product_quantity = request.POST.get('product_quantity')
 
     if not request.session.get('basket'):
-
+        # Adding product to the basket for the first time
         request.session['basket'] = {
             product_name: product_quantity
         }
@@ -28,13 +34,39 @@ def basket_view(request):
         return render(request, 'orders/basket.html', context)
 
     else:
+        # Update the basket or add new items to the basket
         basket = request.session.get('basket')
+        number_of_products_is_stock = Product.objects.get(name=product_name).number_of_product
+
+        if int(product_quantity) > number_of_products_is_stock or int(product_quantity) > 100:
+            product_names = basket.keys()
+            messages.error(request, 'More than inventory.')
+            context['products_in_basket'] = Product.objects.filter(name__in=product_names)
+            return render(request, 'orders/basket.html', context)
+
+        # Add new item to basket
         basket[product_name] = product_quantity
         request.session.modified = True
         product_names = basket.keys()
-
-        messages.success(request, 'Your cart has been updated.')
-
-        print(request.session.get('basket'))
+        messages.success(request, 'Your basket has been updated.')
         context['products_in_basket'] = Product.objects.filter(name__in=product_names)
         return render(request, 'orders/basket.html', context)
+
+
+@csrf_exempt
+def delete_item_from_basket(request):
+    """
+    Takes the product ID and removes it from the basket
+    """
+    if request.method == 'POST':
+        product_name = Product.objects.get(pk=request.POST.get('productId'))
+        basket = request.session.get('basket')
+        basket.pop(str(product_name))
+        request.session.modified = True
+        return HttpResponse('Item deleted')
+
+
+@login_required
+def checkout(request):
+    print(request.session.get('basket'))
+    return HttpResponse('Fuck You :D')
