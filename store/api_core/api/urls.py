1f60c5d606d312ac61882de0d0adf508714ca147
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-10-03 00:00:28 +0330

    :goal_net: Order api urls added

--- store/api_core/api/urls.py
+++ store/api_core/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('customers.api.urls')),
     path('', include('products.api.urls')),
+    path('', include('orders.api.urls')),
 ]
