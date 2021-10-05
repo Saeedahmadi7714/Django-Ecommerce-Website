421b8e3811f100ae110567d7c3d6929ca4890153
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-10-05 22:50:13 +0330

    :sparkles: Offercode's  ajax completed

--- store/products/static/products/js/custom.js
+++ store/products/static/products/js/custom.js
@@ -65,12 +65,10 @@ $(document).ready(function () {
             cache: false,
             timeout: 600000,
             success: function () {
-                console.log("SUCCESS :");
                 alert('Your password was successfully changed.')
                 window.location = '/customer/sign_in/';
             },
             error: function (error) {
-                console.log("ERROR : ", error);
                 $(".alert").remove();
                 $("#error").prepend(`<div class="col-xl-12 alert alert-danger d-flex justify-content-center" role="alert">${error['responseJSON'][Object.keys(error['responseJSON'])[0]]}</div>`)
 
@@ -116,14 +114,12 @@ $(document).ready(function () {
             cache: false,
             timeout: 600000,
             success: function () {
-                console.log("SUCCESS :");
                 $(".alert").remove();
                 $("#error").prepend(`<div class="col-xl-12 alert alert-success d-flex justify-content-center" role="alert">Thanks for your message <p> &#128515;
  </p> </div>`)
 
             },
             error: function (error) {
-                console.log("ERROR : ", error);
 
             }
         });
@@ -152,7 +148,6 @@ $(document).ready(function () {
             cache: false,
             timeout: 600000,
             success: function (response) {
-                console.log("SUCCESS :");
 
                 let result = response.results
 
@@ -196,13 +191,11 @@ $(document).ready(function () {
                         </div>
 `)
 
-                    console.log(result[each])
                 }
 
             },
 
             error: function (error) {
-                console.log("ERROR : ", error);
 
             }
         });
@@ -242,14 +235,57 @@ $(document).ready(function () {
             cache: false,
             timeout: 600000,
             success: function () {
-                console.log("SUCCESS :");
                 $(`table tr#${productId}`).remove();
 
             },
             error: function (error) {
-                console.log("ERROR : ", error);
+
             }
         });
 
     });
 });
+
+
+// Offer code
+$(document).ready(function () {
+
+    $("#applyBtn").unbind().click(function () {
+
+        const offerCode = $('#offerCode').val()
+
+        const $crf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
+        console.log($crf_token)
+        const formData = new FormData(); // Create form data here
+        formData.append('offerCode', offerCode);
+
+        $.ajax({
+            type: "POST",
+            // Adding CSRFToken to headers
+            headers: {"X-CSRFToken": $crf_token},
+            url: "/api/v1/offer_code/",
+            data: formData,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (response) {
+                const offerCodeAmount = response['amount']
+                const splicedTotalPrice = $('#totalPrice').text().split(':')[1].split('$')[1];
+                const offer = splicedTotalPrice * offerCodeAmount / 100
+                const totalPriceAfterOffer = (splicedTotalPrice - offer).toFixed(2)
+                $('#totalPrice').text(`Total price : $${totalPriceAfterOffer}`)
+
+                // Disable apply button if offer code successfuly applied
+                $("#applyBtn").attr('disabled', true);
+            },
+            error: function (error) {
+                $(".alert").remove();
+                $("#error").prepend(`<div class="col-xl-12 alert alert-danger d-flex justify-content-center" role="alert">${error['responseJSON'][Object.keys(error['responseJSON'])[0]]}</div>`)
+
+            }
+        });
+    });
+});
+
+
