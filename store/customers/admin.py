a8b6ad3568f7cadffb38e11e88d693a37291e8a3
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-10-09 03:37:03 +0330

    :passport_control: Update Customer admin in admin.py

--- store/customers/admin.py
+++ store/customers/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import (Customer, )
+from .models import Customer
+
+UserAdmin.fieldsets[1][1]['fields'] = (
+    'first_name',
+    'last_name',
+    'email',
+    # Customer model's custom field
+    'phone_number',
+)
+
+# Admin can not change user account information
+UserAdmin.readonly_fields = (
+    'username',
+    'first_name',
+    'last_name',
+    'email',
+    'phone_number',
+    'last_login',
+    'date_joined'
+)
+
+UserAdmin.list_display += ('phone_number',)
 
 admin.site.register(Customer, UserAdmin)
