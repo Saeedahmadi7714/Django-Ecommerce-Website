75d1c7341b234bada5f4e5e61ec10a6d32b77d98
Saeed ahmadi <saeedahmadi7714@gmail.com>
2021-11-07 18:31:00 +0330

    :truck: urls.py file updated

--- store/conf/urls.py
+++ store/conf/urls.py
@@ -19,15 +19,18 @@ urlpatterns = [
 
     # Django urls for reset password
     path('password_reset/',
-         PasswordResetView.as_view(template_name='customers/password_reset.html'),
+         PasswordResetView.as_view(
+             template_name='customers/password_reset.html'),
          name='password_reset'),
 
     path('password_reset/done/',
-         PasswordResetDoneView.as_view(template_name='customers/password_reset_done.html'),
+         PasswordResetDoneView.as_view(
+             template_name='customers/password_reset_done.html'),
          name='password_reset_done'),
 
     path('reset/<uidb64>/<token>/',
-         PasswordResetConfirmView.as_view(template_name='customers/password_reset_confirm.html'),
+         PasswordResetConfirmView.as_view(
+             template_name='customers/password_reset_confirm.html'),
          name='password_reset_confirm'),
 
     path('reset/done/', PasswordResetCompleteView.as_view(template_name='customers/password_reset_complete.html'),
@@ -35,4 +38,5 @@ urlpatterns = [
 
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
